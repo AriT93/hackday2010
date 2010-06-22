22b38bd791a72ef55169fce85bed8ca9eaf1e895
AriT93 <arit93@yahoo.com>
2010-06-22 11:07:51 -0500

    almost face booked

--- hackday.rb
+++ hackday.rb
@@ -42,15 +42,6 @@ get '/' do
   haml :index
 end
 
-get '/receiver' do
-  %[<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-  <html xmlns="http://www.w3.org/1999/xhtml" >
-  <body>
-      <script src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/XdCommReceiver.js" type="text/javascript"></script>
-  </body>
-  </html>]
-end
 get '/css/style.css' do
   content_type 'text/css'
   sass :style
