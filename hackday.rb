80edded31c8c7cac96a5f37cfd3c225ac84b7517
AriT93 <arit93@yahoo.com>
2010-06-22 13:29:30 -0500

    yay facebooked now tweaking

--- hackday.rb
+++ hackday.rb
@@ -34,7 +34,7 @@ end
 
 get '/' do
   redirect '/login' unless logged_in?
-  redirect "/users/#{current_user.id}/edit" unless current_user.email
+  redirect "/users/#{current_user.id}/edit" unless current_user
   @user = HdUser.first(:email => current_user.email)
   if @user == nil
     @user = HdUser.new(:email => current_user.email)
