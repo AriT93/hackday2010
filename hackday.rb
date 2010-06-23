a121692970ff621fef2b6a146c3421950de46c8e
AriT93 <arit93@yahoo.com>
2010-06-23 09:50:19 -0500

    force linking

--- hackday.rb
+++ hackday.rb
@@ -43,7 +43,7 @@ get '/' do
   if !current_user.email
     current_user.destroy!
     flash[:notice] ="You need to login or create an account first then link it to your Facebook account"
-    redirect '/login'
+    redirect '/logout'
   else
     @user = HdUser.first(:email => current_user.email)
     if @user == nil
