1db57bb3394dc8e75b5b37310bc8e6dab9a98d32
AriT93 <arit93@yahoo.com>
2010-06-23 09:07:14 -0500

    ok looks good now

--- hackday.rb
+++ hackday.rb
@@ -59,6 +59,29 @@ get '/css/style.css' do
   sass :style
 end
 
+get '/connect' do
+  if fb[:user]
+    if current_user.class != GuestUser
+      user = current_user
+    else
+      user = User.get(:fb_uid => fb[:user])
+    end
+
+    if user != nil
+      if !user.fb_uid || user.fb_uid != fb[:user]
+        user.update :fb_uid => fb[:user]
+      end
+      session[:user] = user.id
+    else
+#      user = User.set!(:fb_uid => fb[:user])
+#      session[:user] = user.id
+      flash[:notice] ="You need to login or create an account first then link it to your Facebook account"
+      redirect '/login'
+    end
+  end
+  redirect '/'
+end
+
 get '/login' do
   haml :login
 end
@@ -97,27 +120,4 @@ end
 get '/canvas/fbpolicies' do
   fb2hd
   haml :fbpolicies, :layout => false
-end
-
-get '/connect' do
-  if fb[:user]
-    if current_user.class != GuestUser
-      user = current_user
-    else
-      user = User.get(:fb_uid => fb[:user])
-    end
-
-    if user != nil
-      if !user.fb_uid || user.fb_uid != fb[:user]
-        user.update :fb_uid => fb[:user]
-      end
-      session[:user] = user.id
-    else
-#      user = User.set!(:fb_uid => fb[:user])
-#      session[:user] = user.id
-      flash[:notice] ="You need to create an account first then link it to your Facebook account"
-      redirect '/login'
-    end
-  end
-  redirect '/'
 end
