7df674cd727c7d37521f5f67ded2e22464ef4e95
AriT93 <arit93@yahoo.com>
2010-06-23 08:59:42 -0500

    ok looks good now

--- hackday.rb
+++ hackday.rb
@@ -98,3 +98,26 @@ get '/canvas/fbpolicies' do
   fb2hd
   haml :fbpolicies, :layout => false
 end
+
+get '/connect' do
+  if fb[:user]
+    if current_user.class != GuestUser
+      user = current_user
+    else
+      user = User.get(:fb_uid => fb[:user])
+    end
+
+    if user
+      if !user.fb_uid || user.fb_uid != fb[:user]
+        user.update :fb_uid => fb[:user]
+      end
+      session[:user] = user.id
+    else
+#      user = User.set!(:fb_uid => fb[:user])
+#      session[:user] = user.id
+      flash[:notice] ="You need to create an account first then link it to your Facebook account"
+      redirect '/login'
+    end
+  end
+  redirect '/'
+end
