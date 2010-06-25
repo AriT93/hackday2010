bfeccfdd3f485706401e8732f099166389057376
AriT93 <arit93@yahoo.com>
2010-06-24 20:56:21 -0500

    autopopulate data for new accounts

--- lib/user.rb
+++ lib/user.rb
@@ -9,4 +9,28 @@ class HdUser
   has n, :policies
   has n, :messages
   has n, :claims
+
+  after :create, :populate
+
+
+  def populate
+    ptype = %w{ auto life umbrella fire renter}
+    cstat = %w{ pending review closed canceled open }
+    5.times do
+      a = self.accounts.new
+      a.balance = rand(3000.50)
+
+      p = self.policies.new
+      p.policyType = ptype[rand(4)]
+
+      c = self.claims.new
+      c.status = cstat[rand(4)]
+      c.amount = rand(600.98)
+
+      m = self.messages.new
+      m.text = "this is a random message"
+      m.from = "The Boss"
+      m.subject = "test message"
+    end
+  end
 end
