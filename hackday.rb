e57d54f0c76524fb9b2952781a60b738db2c1c94
AriT93 <arit93@yahoo.com>
2010-06-25 09:21:35 -0500

    merge

--- hackday.rb
+++ hackday.rb
@@ -77,6 +77,7 @@ get '/canvas/' do
   if fb[:user]
     @email = DmUser.first(:fb_uid => fb[:user].to_s)
     @user = HdUser.first(:email => @email.email)
+    @userinfo = fb.users.getInfo :uid => 22909064, :fields => [:name]
   end
   haml :fbook2, :layout => false
 end
