3cc608690c7b45342a76fa124e72ec4ff0c6dd2f
AriT93 <arit93@yahoo.com>
2010-06-27 22:17:53 -0500

    playing

--- hackday.rb
+++ hackday.rb
@@ -61,7 +61,7 @@ get '/' do
 end
 
 get '/sessions/create' do
-  access_token_hash = MiniFB.oauth_access_token(123673737674492,"http://hackday.turetzky.org/sessions/create",'ca85c1a9f608e112de2eb97e7cee67e5', params[:code])
+  access_token_hash = MiniFB.oauth_access_token(123673737674492,"http://hackday.turetzky.org/sessions/create",ca85c1a9f608e112de2eb97e7cee67e5, params[:code])
   @access_token = access_token_hash["acess_token"]
   env[:access_token] = @access_token
   redirect '/'
